788608b314c2e88155da07d9bec918e3f323e962
cardoso <gabrielmtg2@gmail.com>
2025-03-20 12:31:39 -0300

    done

--- count_neg.c
+++ count_neg.c
@@ -1,1 +1,26 @@
-#include <stdio.h> 
+
+/*
+
+Escrever um algoritmo que lê 5 valores para a, um de cada vez, 
+e conta quantos destes valores são negativos, escrevendo esta informação.
+
+*/
+
+
+#include <stdio.h>;
+
+int main() {
+    int a [5];
+    int size = sizeof(a) / sizeof(a[0]);
+    int count = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (a[i] < 0) {
+            count ++;
+        }
+    }
+    printf("there are %d negative numbers", count);
+    return 0;
+}
+
+
