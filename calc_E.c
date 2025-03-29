c701d8e14055715efa386689952f6ba6d9aa7f11
cardoso <gabrielmtg2@gmail.com>
2025-03-29 16:11:23 -0300

    first commit

---
+++ calc_E.c
@@ -0,0 +1,19 @@
+
+
+/*
+Escrever um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o valor de E:
+E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+int N;
+printf("Enter a valid positive number: ");
+scanf("%d", &N);
+
+
+    return 0;
+}
