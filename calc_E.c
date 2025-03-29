75ad8aebb1e4820a18f2cfb2ef162365bd4da317
cardoso <gabrielmtg2@gmail.com>
2025-03-29 16:28:06 -0300

    Validações de input

--- calc_E.c
+++ calc_E.c
@@ -9,11 +9,20 @@ E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!
 
 #include <stdio.h>
 
-int main() {
-int N;
-printf("Enter a valid positive number: ");
-scanf("%d", &N);
 
+    int main() {
+        int N;
 
-    return 0;
-}
+        printf("Enter a valid positive number: ");
+        if(scanf("%d", &N) != 1) {
+            printf("User input is not a valid positive number");
+            return 1;
+        }
+        if (N < 0) {
+            printf("User input is not a valid positive numer");
+            return 1;
+        }
+
+        printf("you've got it right");
+        return 0;
+    }
