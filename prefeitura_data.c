f19fae9958851e3a2cb5abdceb4e5083ef6f0d49
cardoso <cardoso.gab@outlook.com>
2025-04-06 16:43:17 -0300

    adicionando media de todos os salários

--- prefeitura_data.c
+++ prefeitura_data.c
@@ -18,7 +18,9 @@ int main () {
     float maiorSalario = 0;
     int ate100 = 0;
     int ate100_percent = 0;
-    int salario_length = sizeof salario / sizeof salario[0];
+    float salario_length = sizeof salario / sizeof salario[0];
+    float salario_total = 0;
+    float salario_med = 0;
 
     printf("write down 15 salaries: ");
     for (int i = 0; i < salario_length; i++) {
@@ -32,6 +34,8 @@ int main () {
         if(salario[i] <= 100) {
             ate100++;
         }
+
+        salario_total += salario[i];
     }
 
     for (int j = 0; j < salario_length; j++) {
@@ -40,8 +44,9 @@ int main () {
 
     
     ate100_percent = (ate100 * 100) / salario_length;
-    printf("Maior salario: %.2f\n", maiorSalario);
+    salario_med = salario_total / salario_length;
     printf("The percentual of salaries that are under or equal R$100,00 is %d percent\n", ate100_percent);
+    printf("The media of all salaries are: R$%.2f \n", salario_med);
 
     return 0; 
 }
