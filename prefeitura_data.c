524b03a0ae48b06172dbe1168113e097f96eea77
cardoso <cardoso.gab@outlook.com>
2025-04-06 16:33:19 -0300

    adding percentual to the salaries under 100

--- prefeitura_data.c
+++ prefeitura_data.c
@@ -17,6 +17,7 @@ int main () {
     int filhos [15];
     float maiorSalario = 0;
     int ate100 = 0;
+    int ate100_percent = 0;
     int salario_length = sizeof salario / sizeof salario[0];
 
     printf("write down 15 salaries: ");
@@ -32,13 +33,15 @@ int main () {
             ate100++;
         }
     }
-    
+
     for (int j = 0; j < salario_length; j++) {
         printf("Salarie -- R$:%.2f\n", salario[j]);
     }
 
+    
+    ate100_percent = (ate100 * 100) / salario_length;
     printf("Maior salario: %.2f\n", maiorSalario);
-    printf("There is %d salaries that are under or equal R$100,00\n", ate100);
+    printf("The percentual of salaries that are under or equal R$100,00 is %d percent\n", ate100_percent);
 
     return 0; 
 }
