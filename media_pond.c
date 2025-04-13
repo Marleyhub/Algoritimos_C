8ba3f5c15a2f16dd87bd5a1e15813c6bc106d9f7
cardoso <cardoso.gab@outlook.com>
2025-04-13 00:02:48 -0300

    first commit

--- media_pond.c
+++ media_pond.c
@@ -3,3 +3,26 @@
 duas restantes, 3. Mostre o código do aluno, suas três notas, a média calculada e uma mensagem "APROVADO" se a média for maior ou igual a 5 e "REPROVADO" se a
 média for menor que 5. Repita a operação até que o código lido seja negativo.
 */
+
+#include <stdio.h>
+#define SIZE 3
+
+int main(){
+
+    int code;
+    int i = 1;
+    float note[SIZE];
+    int weight;
+
+    printf ("Write down the student code:\n");
+    scanf("%d", &code);
+    while (i <= SIZE) {
+        printf("Write down the note %d\n", i);
+        scanf("%f", &note[i]);
+        printf("%.2f\n", note[i]);
+        i++;
+    }
+    
+   
+    return 0;
+}
