4b549d5496716f3709f9f49c7fcf3d2224e40866
cardoso <cardoso.gab@outlook.com>
2025-04-19 16:24:29 -0300

    back again

---
+++ ari_prog.c
@@ -0,0 +1,20 @@
+
+/*
+11. Escreva um algoritmo que leia um número n (número de termos de uma progressão aritmética),
+a1 ( o primeiro termo da progressão) e r (a razão da progressão) e
+escreva os n termos desta progressão, bem como a soma dos elementos.
+*/
+int main (){
+
+    int n, a1, r; 
+    int i = 0;
+    int *array;
+
+    printf("How long should be the progresson: \n"); scanf("%d", &n);
+    while(i < n) {
+    
+        int prog = a1 + prog;
+
+    }
+    return 0;
+}
