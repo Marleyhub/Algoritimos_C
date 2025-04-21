75890e1b6432af7a28a1d3f3a0c7cb35e264fc16
cardoso <cardoso.gab@outlook.com>
2025-04-21 20:33:24 -0300

    fixing limits of arrays and loops

--- tabuada.c
+++ tabuada.c
@@ -15,15 +15,32 @@ n x n = n^2
 
 int main () {
 
-    int array[SIZE];
-    int i, j;
+    int n[SIZE];
+    int *tabuadas[20];
+    int i = 0;
+    int j;
 
     printf("write down 20 number to make a multiplication table: \n");
 
-    for (i = 0; i < SIZE; i++) {
-        scanf("Number-%d: %d", i + 1, array[i]);
-        printf("%d", array[i]);
+    while(i < SIZE) {
+        printf("Number-%d: ", i + 1);
+        scanf("%d", &n[i]);
+
+        tabuadas[i] = (int *)malloc(n[i] * sizeof(int));
+        
+        if(tabuadas[i] == NULL){
+            printf("Memory alocation for tabuada faild\n");
+            return 1;
+        }
+
+        for (j = 0; j < n[i]; j++){
+            tabuadas[i][j] = n[i] * (j + 1);
+            // printf("%d\n", tabuadas[i][j]);
+        }
+        i++;
     }
+
+
 
     return 0; 
 }
