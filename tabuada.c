4192e97554bf551214f2c53c29eb34cc3073e7aa
cardoso <cardoso.gab@outlook.com>
2025-04-21 19:19:26 -0300

    fixing question

--- tabuada.c
+++ tabuada.c
@@ -1,11 +1,11 @@
 /*
 12. Escrever um algoritmo que leia 20 valores para uma variável n e,
 para cada um deles, calcule a tabuada de 1 até n. Mostre a tabuada na forma:
-x n = n
+1 x n = n
 2 x n = 2n
 3 x n = 3n
 .......
-n x n = n2
+n x n = n^2
 */ 
 
 #include <stdio.h>
