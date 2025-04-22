ee8f26368d2f49507e1840ef75da9c0e32561d66
cardoso <cardoso.gab@outlook.com>
2025-04-22 19:11:32 -0300

    working version

--- fatorial.c
+++ fatorial.c
@@ -3,8 +3,49 @@
 fatorial deste valor.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
+    int i = 0;
+    int size = 0;
+    int *numbers = NULL;
+    unsigned long long *fatored = malloc(size * sizeof(unsigned long long));
+
+    printf("Write down how many fatorials do you want: \n");
+    scanf("%d", &size);
+
+    numbers = (int *)malloc(size * sizeof(int));
+    fatored = (unsigned long long *)malloc(size * sizeof(unsigned long long));
+
+    if(numbers == NULL || fatored == NULL){
+        printf("Memory allocation faild");
+        return 1;
+    }
+
+    printf("Write down the numbers that you want to have the fatorial: \n");
+
+    while (i < size){
+        printf("Number-%d:", i + 1);
+        scanf("%d", &numbers[i]);
+        i++;
+    }
+
+    for (int j = 0; j < size; j++){
+        unsigned long long fatorial = 1;
+        for (int k = 1; k <= numbers[j]; k++){
+            fatorial *= k;
+        }
+        fatored[j] = fatorial;
+    }
     
+    printf("**********results*********\n");
+    for (i = 0; i < size; i++){
+        printf("%d! = %llu\n", numbers[i], fatored[i]);
+    }
+
+    free(numbers);
+    free(fatored);
     
     return 0; 
 }
