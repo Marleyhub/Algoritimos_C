9aeb6452abce469c772207dc3604dbbf0a7d957b
cardoso <cardoso.gab@outlook.com>
2025-04-21 21:05:04 -0300

    first commit

--- fatorial.c
+++ fatorial.c
@@ -2,3 +2,9 @@
 13. Escrever um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir. Para cada número lido, mostre uma tabela contendo o valor lido e o
 fatorial deste valor.
 */
+
+int main(){
+    
+    
+    return 0; 
+}
