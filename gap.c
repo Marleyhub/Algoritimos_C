5131c6e8116ba4b62c29ee35be8036d1a8f69264
cardoso <cardoso.gab@outlook.com>
2025-04-26 02:39:34 -0300

    fisrt commit

---
+++ gap.c
@@ -0,0 +1,23 @@
+/*
+15. Escrever um algoritmo que leia uma quantidade desconhecida de números e conte quantos
+ deles estão nos seguintes intervalos: [0.25], [26,50], [51,75] e [76,100]. A
+entrada de dados deve terminar quando for lido um número negativo.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int *numbers = NULL;
+    int size = 1; 
+    int capacity = 2;
+    int count_0_25 = 0;
+    int count_26_50 = 0;
+    int count_51_75 = 0;
+    int count_76_100 = 0;
+    
+    while(1) {
+
+        return 0;
+    }
+}
