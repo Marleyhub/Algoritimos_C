b4cb1a3ddcb0a84c85593d71c6d3605021fa96df
cardoso <cardoso.gab@outlook.com>
2025-04-26 03:50:59 -0300

    first commit

---
+++ q_c_raiz.c
@@ -0,0 +1,16 @@
+/*
+16. Escrever um algoritmo que lê um conjunto não determinado de valores, um de cada vez,
+ e escreve uma tabela com cabeçalho, que deve ser repetido a cada 20 linhas. A
+tabela conterá o valor lido, seu quadrado, seu cubo e sua raiz quadrada.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    return 0;
+}
+
+
