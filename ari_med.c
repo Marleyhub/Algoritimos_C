06d694ccab587930275f97abbcbb0aedfcfae912
cardoso <cardoso.gab@outlook.com>
2025-05-05 19:15:28 -0300

    testing changes

--- ari_med.c
+++ ari_med.c
@@ -4,6 +4,7 @@
 /*
 Escreva um algoritmo que calcule a média aritmética das 3 notas dos alunos de uma classe. O algoritmo deverá ler, além das notas, o código do aluno e deverá ser
 encerrado quando o código for igual a zero.
+
 */
 #include <stdio.h>
 
