830437cf7723b4a64b6e2437fa9eafdce62f0005
cardoso <cardoso.gab@outlook.com>
2025-08-22 17:29:45 -0300

    pontos no plano with math

--- PUCSP/pontos_no_plano.c
+++ PUCSP/pontos_no_plano.c
@@ -5,6 +5,15 @@ escreva a distância entre eles. A fórmula que efetua tal cálculo é:
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+
+float math_func(float x1, float y1, float x2, float y2) {
+
+    float distance;
+    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    return distance;
+}
 
 int main(){
     typedef struct 
@@ -15,14 +24,18 @@ int main(){
     Ponto p1;
     Ponto p2;
 
-    p1.x = 10;
-    p1.y = 20.1;
+    p1.x = 5.99;
+    p1.y = 400.46;
 
-    p2.x = 23;
-    p2.y = 1;
+    p2.x = 34.75;
+    p2.y = 18.98;
 
-    printf("p1 = (%f, %f)\n", p1.x, p1.y);
-    printf("p1 = (%f, %f)", p2.x, p2.y);
-    
+    float d;
+
+    d = math_func(p1.x, p1.y, p2.x, p2.y);
+
+    printf("The distance between the points is: %f", d);
+
     return 0;
 }
+
