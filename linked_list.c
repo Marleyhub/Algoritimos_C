9cf6a0076ad2dfcb2220c1ac47d311cc97f04788
cardoso <cardoso.gab@outlook.com>
2025-09-05 17:18:13 -0300

    linked list

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 31
+#define SIZE 30
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+} node;
+
+int main(){
+    node *list = NULL;
+
+    for (int i = 0; i < SIZE; i++){
+        node *n = malloc(sizeof(node));
+
+        if(n == NULL){
+            return 1;
+        }
+
+        n->value = i + 1;
+        n->next = NULL;
+
+        if(list == NULL){
+            list = n;
+
+        } else {
+            for(node *ptr = list; ptr != NULL; ptr = ptr->next){
+                if (ptr->next == NULL){
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
+
+    node *ptr = list;
+    while(ptr->next != NULL){
+        printf("%d\n", ptr->value);
+        ptr = ptr->next;
+    }
+}
