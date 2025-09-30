b7c05e0dcfa71813847884aab45d5ea2a775899d
cardoso <cardoso.gab@outlook.com>
2025-09-29 23:55:08 -0300

    trying new list

--- linked_list.c
+++ linked_list.c
@@ -1,48 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 30
+#define SIZE 10
 
-typedef struct node
-{
+// always create the node struct outside any func
+// next should be always a pointer to struct type Node
+typedef struct Node{
     int value;
-    struct node *next;
-} node;
-
-node *list = NULL;
+    struct Node* next;
+}Node;
 
 int createList(){
-    for (int i = 0; i < SIZE; i++){
-        node *n = malloc(sizeof(node));
-        if(n == NULL){
-            return 1;
-        }
-
-        n->value = i + 1;
-        n->next = NULL;
-
-        if(list == NULL){
-            list = n;
-
-        } else {
-            for(node *ptr = list; ptr != NULL; ptr = ptr->next){
-                if (ptr->next == NULL){
-                    ptr->next = n;
-                    break;
-                }
-            }
-        }
+    int* list = malloc(SIZE * sizeof(int)); 
+    Node* node = malloc(sizeof(Node));
+    printf("%d",list[0]);
+    for(int i = 0; i < SIZE; i++){
+        node->value = i;
+        node->next = NULL;
     }
 }
 
-void printlist(){
-    node *ptr = list;
-    while(ptr->next != NULL){
-        printf("%d\n", ptr->value);
-        ptr = ptr->next;
-    }
-}
-int main(){
-    createList();
-    printlist(); 
+
+int mais() {
+    creteList();
+    return 0;
 }
