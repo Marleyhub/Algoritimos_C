06845a0a590a0c7d54d7130695af0dbf283e42b1
cardoso <cardoso.gab@outlook.com>
2025-09-30 01:49:33 -0300

    reversed already blindly done

--- linked_list.c
+++ linked_list.c
@@ -7,21 +7,46 @@
 // next should be always a pointer to struct type Node
 typedef struct Node{
     int value;
-    struct Node* next;
+    struct Node* next; 
 }Node;
 
-int createList(){
-    int* list = malloc(SIZE * sizeof(int)); 
-    Node* node = malloc(sizeof(Node));
-    printf("%d",list[0]);
-    for(int i = 0; i < SIZE; i++){
-        node->value = i;
-        node->next = NULL;
+// Alocates a space for a empty(trash value) node
+// Sets values
+// Sets next like pointer to node param at first NULL 
+// returns pointer to a Node
+Node* createRList(Node* head, int i){ 
+    Node* newNode = malloc(sizeof(Node));
+    newNode->value = i;
+    newNode->next = head;
+    return newNode;
+}
+
+Node* createList(Node* head, int i){ 
+    Node* newNode = malloc(sizeof(Node));
+    newNode->value = i;
+    newNode->next = head;
+    return newNode;
+}
+
+// Passing the last node created and settig it like current to print
+// While dont find next == NULL (tail) print value then changes current to next
+void printList(Node* node){
+    Node* currentNode = node;
+    while(node->next != NULL){
+        printf("%d\n", currentNode->value);
+        currentNode = currentNode->next;
     }
 }
 
+// Head should be NULL only to iniciate list
+// Every call to createList() returns newNode to head
+int main() {
+    Node* head = NULL;
 
-int mais() {
-    creteList();
+    for (int i = 0; i < SIZE; i++){
+        head = createRList(head, i);
+    }
+
+    printList(head);
     return 0;
 }
