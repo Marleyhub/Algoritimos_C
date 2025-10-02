1a6ed8fd52ee074c6cc438acaa0d75b4d0b5f4fa
cardoso <cardoso.gab@outlook.com>
2025-10-02 02:22:02 -0300

    stack implemented

--- stack.c
+++ stack.c
@@ -3,6 +3,46 @@
 
 #define SIZE 30
 
-int main(){
+typedef struct Node{
+    int value;
+    struct Node* next;
+}Node;
 
+
+void stack(Node** top, Node** botton, int i){
+    Node* newNode = malloc(sizeof(Node));
+
+    if(!newNode){
+        printf("Memory allocation faild");
+        return;
+    }
+
+    newNode->value = i;
+    newNode->next = *top;
+
+    if(*top == NULL){
+        *top = *botton = newNode;
+        return;
+    }
+
+    *top = newNode;
+    return;
+}
+
+void printStack(Node* top){
+    Node* current = top;
+    while(current->next != NULL){
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
+int main(){
+    Node* top = NULL;
+    Node* botton = NULL;
+
+    for(int i = 0; i < SIZE; i++){
+        stack(&top, &botton, i);
+    }
+
+    printStack(top);
 }
