75f043cfe7828d576ae3c7519d6f0b761b37e5e7
cardoso <cardoso.gab@outlook.com>
2025-10-02 01:27:32 -0300

    queue implement without help

---
+++ new_queue.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 30
+
+typedef struct Node{
+    int value;
+    struct Node* next;
+}Node;
+
+void enqueue(Node** front, Node** rear, int i){
+    Node* newNode = malloc(sizeof(Node));
+
+    if(!newNode){
+        printf("Memory allocation faild");
+        return;
+    }
+
+    newNode->value = i;
+    newNode->next = NULL;
+
+    if(*front == NULL){
+        *front = *rear = newNode;
+        return;
+    }
+
+    (*rear)->next = newNode;
+    *rear = newNode;
+    return;
+}
+
+void printQueue(Node* front){   
+    Node* current = front;
+    while(current != NULL){
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
+
+int main(){
+    Node* front = NULL;
+    Node* rear =  NULL;
+
+    for (int i = 0; i < SIZE; i++){
+        enqueue(&front, &rear, i);
+    }   
+
+    printQueue(front);
+
+    return 0;
+}
---
+++ stack.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 30
+
+int main(){
+
+}
