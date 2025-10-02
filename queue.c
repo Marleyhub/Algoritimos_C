85fa5728053f1ea2732953e4f6e30314f466a84b
cardoso <cardoso.gab@outlook.com>
2025-10-01 23:12:57 -0300

    unqueue properly done

--- queue.c
+++ queue.c
@@ -3,51 +3,51 @@
 
 #define SIZE 30
 
+// always create the node struct outside any func
+// next should be always a pointer to struct type Node
 typedef struct Node{
     int value;
     struct Node* next;
 }Node;
 
-// why i need to pass "**" and in the linked list i dont
-void enqueue(Node** front, Node** rear, int i){
+Node* enqueue(Node** front, Node* rear, int i){
     Node* newNode = malloc(sizeof(Node));
 
     if(!newNode){
-        printf("Memory allocation failed\n");
-        return;
+        printf("Memory allocation faild");
+        return NULL;
     }
 
     newNode->value = i;
     newNode->next = NULL;
 
-    // dont understand why the "*" before pointer
-    if(*rear == NULL){
-        *front = *rear = newNode;
-    } else {
-        // ok i know i need "()" to call insede value but what is it really doing?
-        (*rear)->next = newNode;
-        *rear = newNode;
+    if(*front == NULL){
+        *front = newNode;
+        rear = *front;
+        return rear;
     }
 
+    rear->next = newNode;
+    return newNode;
 }
 
 void printQueue(Node* front){
     Node* current = front;
     while(current != NULL){
         printf("%d\n", current->value);
-        current = current->next;  
+        current = current->next;
     }
 }
 
 int main(){
+
     Node* front = NULL;
     Node* rear = NULL;
 
-    for (int i = 0; i < SIZE; i++){
-           // Passing the adress of this pointers but i dont get the logic
-           enqueue(&front, &rear, i);
-    } 
-    printQueue(front);
+    for(int i = 0; i < SIZE; i++){
+        rear = enqueue(&front, rear, i);
+    }
 
+    printQueue(front);
     return 0;
 }
