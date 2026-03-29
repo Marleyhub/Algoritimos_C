01f2dfed2307c58cd6934ea660f046e19f7240ef
Marleyhub <cardoso.gab@outlook.com>
2026-03-29 20:08:08 -0300

    node initialization

---
+++ b_tree.c
@@ -0,0 +1,23 @@
+/*  
+This file consistes in my attempt to write a binary tree.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Node
+struct Node{
+    int key;
+    struct node *left;
+    struct node *rigth; 
+};
+
+struct Node* createNode(int item) {
+    struct Node *tmp = (struct Node*)malloc(sizeof(struct Node));
+    tmp -> key = item;
+    tmp -> rigth =  NULL;
+    tmp -> left = NULL;
+
+    return tmp;
+}
